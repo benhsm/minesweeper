43ffe10951f7886d84123185b62d294930088607
Benjamin Ang <ben.ang@fastmail.com>
2022-05-05 20:17:14 -0400

    Implement basic game loop and ui

---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var bolded = lipgloss.NewStyle().Bold(true)
+var flag = lipgloss.NewStyle().Foreground(lipgloss.Color("9")) // Red
+var ok = lipgloss.NewStyle().Foreground(lipgloss.Color("10"))  // Green
+
+const (
+	hidden = iota
+	revealed
+	flagged
+)
+
+type point struct {
+	x int
+	y int
+}
+
+type tile struct {
+	char  string
+	state int
+}
+
+type mineField [][]tile
+
+type model struct {
+	field  mineField
+	cursor point
+}
+
+func initialModel() model {
+	mineField := newMineField(newField(9, 9, 10))
+	return model{
+		field:  mineField,
+		cursor: point{0, 0},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor.y > 0 {
+				m.cursor.y--
+			}
+		case "down", "j":
+			if m.cursor.y < len(m.field) {
+				m.cursor.y++
+			}
+		case "left", "h":
+			if m.cursor.x > 0 {
+				m.cursor.x--
+			}
+		case "right", "l":
+			if m.cursor.x < len(m.field[0]) {
+				m.cursor.x++
+			}
+		case "enter":
+			m.field[m.cursor.y][m.cursor.x].state = revealed
+			if m.field[m.cursor.y][m.cursor.x].char == "☀" {
+				return m, tea.Quit
+			}
+		case " ":
+			switch m.field[m.cursor.y][m.cursor.x].state {
+			case flagged:
+				m.field[m.cursor.y][m.cursor.x].state = hidden
+			case hidden:
+				m.field[m.cursor.y][m.cursor.x].state = flagged
+			case revealed:
+				m.field[m.cursor.y][m.cursor.x].state = revealed
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Minesweeper! \n"
+	s += "Use HJKL to move cursor, enter to reveal, space to flag\n"
+	for y, row := range m.field {
+		for x, col := range row {
+			c := ""
+			if col.state == hidden {
+				c = fmt.Sprintf(" %s ", "?")
+			} else if col.state == revealed {
+				c = fmt.Sprintf(" %s ", col.char)
+				c = ok.Render(c)
+			} else if col.state == flagged {
+				c = fmt.Sprintf(" %s ", "")
+				c = flag.Render(c)
+			}
+			if x == m.cursor.x && y == m.cursor.y {
+				c = bolded.Render(c)
+			}
+			s += c
+
+		}
+		s += "\n"
+	}
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
--- minesweeper.go
+++ minesweeper.go
@@ -5,7 +5,11 @@ import (
 	"math/rand"
 )
 
-func newField(height, width, mines int) {
+// newField takes dimensions and returns a 2D array
+// representing a randomly generated minesweeper playing field.
+// each cell is filled with either the value -1 representing a mine, or an
+// integer reprsenting the number of adjacent mines.
+func newField(height, width, mines int) [][]int {
 
 	// Make a slice with values ranging from 0 to (height X width) and shuffle it
 	sequence := make([]int, height*width)
@@ -17,29 +21,69 @@ func newField(height, width, mines int) {
 	})
 
 	// Initialize 2D array representing field
-	field := make([][]rune, height)
+	field := make([][]int, height)
 	for i := range field {
-		field[i] = make([]rune, width)
+		field[i] = make([]int, width)
 		for j := range field[i] {
-			field[i][j] = '0'
+			field[i][j] = 0
 		}
 	}
 
+	// Use the random sequence to mine the empty field
 	for i := 0; i < mines; i++ {
 		v := sequence[i]
-		col := v % height // Modulus to get height
-		row := v / height // Integer division to get row
-		field[row][col] = '☀'
+		col := v % width // Modulus to get column position
+		row := v / width // Integer division to get row
+		field[row][col] = -1
 	}
 
-	for _, row := range field {
-		fmt.Printf("\n")
-		for _, col := range row {
-			fmt.Printf("%q", col)
+	// Fill tiles with numbers representing adjacent mines
+	for i := 0; i < height*width; i++ {
+		col := i % width // Modulus to get column position
+		row := i / width // Integer division to get row
+
+		if field[row][col] == -1 {
+			continue
 		}
+
+		val := 0
+		for x := -1; x <= 1; x++ {
+			for y := -1; y <= 1; y++ {
+				posx := col + x
+				posy := row + y
+				if posx >= 0 && posx <= width-1 {
+					if posy >= 0 && posy <= height-1 {
+						if field[posy][posx] == -1 {
+							val += 1
+						}
+					}
+				}
+			}
+		}
+		field[row][col] = val
 	}
+
+	return field
 }
 
-func main() {
-	newField(10, 10, 20)
+func newMineField(field [][]int) mineField {
+	height := len(field)
+	width := len(field[0])
+	result := make(mineField, height)
+	for i := range result {
+		result[i] = make([]tile, width)
+	}
+	for i := 0; i < height*width; i++ {
+		col := i % width // Modulus to get height
+		row := i / width // Integer division to get row
+
+		if field[row][col] == -1 {
+			result[row][col].char = "☀"
+		} else {
+			result[row][col].char = fmt.Sprintf("%d", field[row][col])
+		}
+		result[row][col].state = hidden
+	}
+
+	return result
 }
