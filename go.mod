5a3c0575261c0f9382d6bb17f52f5b2e7d44ad29
Benjamin Ang <ben.ang@fastmail.com>
2022-05-29 22:29:46 -0400

    Change menuModel to use key and help bubbles
    I also added wasd as an alternate directional scheme, and made space a
    selection key in addition to enter. Also changed the menu view to make
    the active selection more obvious.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/benhsm/minesweeper
 go 1.18
 
 require (
+	github.com/charmbracelet/bubbles v0.10.3
 	github.com/charmbracelet/bubbletea v0.20.0
 	github.com/charmbracelet/lipgloss v0.5.0
 )
--- tui/menu.go
+++ tui/menu.go
@@ -3,9 +3,67 @@ package tui
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
+
+type menuKeyMap struct {
+	Up     key.Binding
+	Down   key.Binding
+	Left   key.Binding
+	Right  key.Binding
+	Help   key.Binding
+	Quit   key.Binding
+	Select key.Binding
+}
+
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
+func (k menuKeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+// FullHelp returns keybindings for the expanded help view. It's part of the
+// key.Map interface.
+func (k menuKeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Left, k.Right}, // first column
+		{k.Help, k.Quit, k.Select},      // second column
+	}
+}
+
+var keys = menuKeyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k", "w"),
+		key.WithHelp("↑/k/w", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j", "s"),
+		key.WithHelp("↓/j/s", "move down"),
+	),
+	Left: key.NewBinding(
+		key.WithKeys("left", "h", "a"),
+		key.WithHelp("←/h/a", "change setting"),
+	),
+	Right: key.NewBinding(
+		key.WithKeys("right", "l", "d"),
+		key.WithHelp("→/l/d", "change setting"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+	Select: key.NewBinding(
+		key.WithKeys("enter", "space"),
+		key.WithHelp("enter/space", "select"),
+	),
+}
 
 var menuSelectStyle = lipgloss.NewStyle().Bold(true)
 
@@ -21,6 +79,8 @@ const (
 )
 
 type menuModel struct {
+	keys          menuKeyMap
+	help          help.Model
 	menuSelection int
 	menuChoices   []string
 	difficulty    int
@@ -29,6 +89,8 @@ type menuModel struct {
 
 func newMenuModel() menuModel {
 	return menuModel{
+		keys:        keys,
+		help:        help.New(),
 		menuChoices: []string{"Difficulty: ", "Play"},
 		difficulty:  beginner,
 		inMenu:      true,
@@ -49,39 +111,42 @@ func (m menuModel) view() string {
 			}
 		}
 		if i == m.menuSelection {
-			c = menuSelectStyle.Render(c)
+			c = "<< " + menuSelectStyle.Render(c) + " >> "
 		}
-		s += fmt.Sprintf("%s\n", c)
+		s += fmt.Sprintf("\n%s\n", c)
 	}
+	s += "\n" + m.help.View(m.keys)
 	return s
 }
 
 func (m menuModel) update(msg tea.Msg) (menuModel, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.help.Width = msg.Width
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "k":
+		switch {
+		case key.Matches(msg, m.keys.Up):
 			if m.menuSelection > 0 {
 				m.menuSelection--
 			} else {
 				m.menuSelection = len(m.menuChoices) - 1
 			}
-		case "j":
+		case key.Matches(msg, m.keys.Down):
 			if m.menuSelection < len(m.menuChoices)-1 {
 				m.menuSelection++
 			} else {
 				m.menuSelection = 0
 			}
 
-		case "h":
+		case key.Matches(msg, m.keys.Left):
 			if m.menuSelection == difficultyOption {
 				if m.difficulty > beginner {
 					m.difficulty--
+				} else {
+					m.difficulty = expert
 				}
-			} else {
-				m.difficulty = expert
 			}
-		case "l":
+		case key.Matches(msg, m.keys.Right):
 			if m.menuSelection == difficultyOption {
 				if m.difficulty < expert {
 					m.difficulty++
@@ -89,10 +154,12 @@ func (m menuModel) update(msg tea.Msg) (menuModel, tea.Cmd) {
 					m.difficulty = beginner
 				}
 			}
-		case "enter":
+		case key.Matches(msg, m.keys.Select):
 			if m.menuSelection == playButton {
 				m.inMenu = false
 			}
+		case key.Matches(msg, m.keys.Help):
+			m.help.ShowAll = !m.help.ShowAll
 		}
 	}
 	return m, nil
